2e46ed9c831e9d2c546790c72e056334efff0bca
ynyaki <inaki.bazan.974@gmail.com>
2025-10-22 09:58:53 +0200

    22/10/25 - U2A9

--- src/es/iesso/EjsClase/Unidad2/U2A9.java
+++ src/es/iesso/EjsClase/Unidad2/U2A9.java
@@ -62,53 +62,53 @@ public final class U2A9 extends Impresora {
             ej5();
         else if(n == 6)
             ej6();
-        // TODO Añadir ejercicios
         else
             imprln(ERR_EJ);
     }
 
     private static void ej1() {
 
-        imprln(imprFunc(1) + esCapicua(101));
+        imprln(imprFunc(1) + esCapicua(10201));
         imprent("101");
         linea();
 
-        imprln(imprFunc(2) + esPrimo(119));
-        imprent("119");
+        imprln(imprFunc(2) + esPrimo(113));
+        imprent("113");
         linea();
 
         imprln(imprFunc(3) + siguientePrimo(119));
         imprent("119");
         linea();
-/*
-        imprln(imprFunc(4) + digitoN(125, 1));
-        imprent(125 + ", " + 1);
+
+        imprln(imprFunc(4) + potencia(2, 4));
+        imprent("2, 4");
         linea();
 
-        imprln(imprFunc(5) + digitoN(125, 1));
-        imprent(125 + ", " + 1);
+        imprln(imprFunc(5) + digitos(1000));
+        imprent("1000");
         linea();
-*/
+
         imprln(imprFunc(6) + voltea(221));
         imprent("221");
         linea();
 
-        imprln(imprFunc(7) + digitoN(125, 1));
-        imprent(125 + ", " + 1);
+        imprln(imprFunc(7) + digitoN(150, 1));
+        imprent("150, 1");
         linea();
     }
 
     // Función 1
     private static boolean esCapicua(int n) {
-        return (n == voltea(n));
+        int m = voltea(n);
+        return (n == m);
     }
 
     // Función 2
     private static boolean esPrimo(int n) {
-        boolean primo = false;
+        boolean primo = true;
         for(int i = 2; i < n; i++)
             if (n % i == 0) {
-                primo = true;
+                primo = false;
                 break;
             }
         return primo;
@@ -116,29 +116,30 @@ public final class U2A9 extends Impresora {
 
     // Función 3
     private static int siguientePrimo(int n) {
-        int i = n;
-        while(!esPrimo(n))
+        int i = n + 1;
+        while(!esPrimo(i))
             i++;
         return i;
     }
-/*
+
     // Función 4
-    private static char digitoN(int n, int i) {
-        String num = Integer.toString(n);
-        return num.charAt(i);
+    private static int potencia(int n, int e) {
+        int r = n;
+        for(int i = 1; i < e; i++)
+            r *= n;
+        return r;
     }
 
     // Función 5
-    private static char digitoN(int n, int i) {
-        String num = Integer.toString(n);
-        return num.charAt(i);
+    private static int digitos(int n) {
+        return (Integer.toString(n).length());
     }
-*/
+
     // Función 6
     private static int voltea(int n) {
         String tInic = Integer.toString(n);
         String tFin = "";
-        for(int i = tInic.length() - 1; i > 0; i--)
+        for(int i = tInic.length() - 1; i >= 0; i--)
             tFin = tFin.concat(String.valueOf(tInic.charAt(i)));
         return Integer.parseInt(tFin);
     }
@@ -148,6 +149,56 @@ public final class U2A9 extends Impresora {
         String num = Integer.toString(n);
         return num.charAt(i);
     }
+
+    // Función 8
+    private static int posicionDeDigito(int n, int d) {
+        int i = 0;
+        String nTxt = Integer.toString(n);
+        while(String.valueOf(nTxt.charAt(i)).equals(String.valueOf(d)))
+            i++;
+        return i;
+    }
+
+
+    // Función 9
+    private static int quitaPorDetras(int n, int d) {
+        String num = Integer.toString(n);
+        return Integer.parseInt(num.substring(0, d - 1));
+    }
+
+
+    // Función 10
+    private static int quitaPorDelante(int n, int d) {
+        String num = Integer.toString(n);
+        return Integer.parseInt(num.substring(d));
+    }
+
+
+    // Función 11
+    private static int pegaPorDetras(int n, int x) {
+        return 0;
+    }
+
+
+    // Función 12
+    private static int pegaPorDelante(int n, int x) {
+        return 0;
+    }
+
+
+    // Función 13
+    private static int trozoDeNumero(int n, int i, int f) {
+        String nTxt = String.valueOf(n);
+        return Integer.parseInt(nTxt.substring(i, f));
+    }
+
+
+    // Función 14
+    private static String juntaNumeros(int a, int b) {
+        return String.valueOf(a).concat(String.valueOf(b));
+    }
+
+
 
     private static String imprFunc(int nF) {
         String txtFunc = switch(nF) {
@@ -171,7 +222,7 @@ public final class U2A9 extends Impresora {
     }
 
     private static void imprent(String txtIn) {
-        impr("Entrada original: " + txtIn);
+        imprln("Entrada original: " + txtIn);
     }
 
     private static void ej2() {
